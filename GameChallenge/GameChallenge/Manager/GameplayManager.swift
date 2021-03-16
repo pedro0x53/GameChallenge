9b510b8e6307bb1f2c7a9d720b841585bb685876
Pedro Sousa <pedrosousa2708@gmail.com>
2021-03-16 10:23:50 -0300

    esqueleto da GameplayManager

--- GameChallenge/GameChallenge/Manager/GameplayManager.swift
+++ GameChallenge/GameChallenge/Manager/GameplayManager.swift
@@ -10,14 +10,44 @@ import SpriteKit
 
 class GameplayManager {
 
-//    let entities: Set<GKEntity>
-//    let scene: SKScene
-//    //let statusManager: StatusManager
-//    //let tableManager: TableManager
-//    //let handManager: HandManager
-//
-//    init(scene: SKScene) {
-//
-//    }
+    private var entities = Set<GKEntity>()
+    private let scene: SKScene
 
+    // let statusManager: StatusManager
+    // let tableManager: TableManager
+    // let handManager: HandManager
+
+    init(scene: SKScene) {
+        self.scene = scene
+    }
+
+    func add(entity: GKEntity) {
+        self.entities.insert(entity)
+
+        if let spriteComponent = entity.component(ofType: SpriteComponent.self) {
+            self.scene.addChild(spriteComponent.node)
+        }
+    }
+
+    func remove(entity: GKEntity) {
+        self.entities.remove(entity)
+
+        if let spriteComponent = entity.component(ofType: SpriteComponent.self) {
+            self.scene.removeChildren(in: [spriteComponent.node])
+        }
+    }
+
+    func drawCards() -> [Card] {
+        return []
+    }
+
+    func putCardsOnTheTable(cards: [Card]) {}
+
+    func revealCard() {}
+
+    func takeDamage() {}
+
+    func nextLevel() {}
+
+    func gameOver() {}
 }
