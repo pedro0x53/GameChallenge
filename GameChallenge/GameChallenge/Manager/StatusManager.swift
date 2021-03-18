25d36843cb9e31b83fa55037ae422e1dd0930f14
Gustavo Lemos <gsvieira08@gmail.com>
2021-03-18 11:36:36 -0300

    Salvando basico so pra criar a branch do statusitem

---
+++ GameChallenge/GameChallenge/Manager/StatusManager.swift
@@ -0,0 +1,22 @@
+//
+//  StatusManager.swift
+//  GameChallenge
+//
+//  Created by Gustavo Lemos on 18/03/21.
+//
+
+import GameplayKit
+import SpriteKit
+
+class StatusManager {
+    let gameplayManager: GameplayManager
+    var lifeItem: StatusItem
+    
+    init(manager: GameplayManager) {
+        //iniciar com 3 vidas e 2 reveals
+    }
+    
+    func wrong(){
+        //reduzir vida
+    }
+}
